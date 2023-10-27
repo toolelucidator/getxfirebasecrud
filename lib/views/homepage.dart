18a0b09936a47aa160515665bf28eaa04f11688b
toolelucidator <jorge.tapiaupt@outlook.com>
2023-10-27 10:08:44 -0600

    Modified homepage

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -24,32 +24,32 @@ class HomePage extends StatelessWidget {
               child: homeScreenController.isLoading.value == true
                   ? CircularProgressIndicator()
                   : Obx(()=>
-                    ListView.separated(
-                        itemBuilder: (BuildContext context, index) {
-                          return Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Card(
-                              color: Colors.deepOrangeAccent.shade100,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  Column(
-                                    children: [
-                                      Text(homeScreenController
-                                          .wordList[index].title),
-                                      Text(homeScreenController
-                                          .wordList[index].meaning),
-                                    ],
-                                  )
-                                ],
-                              ),
+                  ListView.separated(
+                      itemBuilder: (BuildContext context, index) {
+                        return Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Card(
+                            color: Colors.deepOrangeAccent.shade100,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(homeScreenController
+                                        .wordList[index].title),
+                                    Text(homeScreenController
+                                        .wordList[index].meaning),
+                                  ],
+                                )
+                              ],
                             ),
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, index) {
-                          return Divider(thickness: 2, color: Colors.black);},
-                        itemCount: homeScreenController.wordList.value.length),
-                  ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, index) {
+                        return Divider(thickness: 2, color: Colors.black);},
+                      itemCount: homeScreenController.wordList.value.length),
+              ),
             ),
           );
         });
